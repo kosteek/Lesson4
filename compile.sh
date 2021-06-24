8d10d158329910ca98afed52d512d1c2fbdd384f
kosteek <kosteek911@gmail.com>
2021-06-24 23:25:31 +0300

    update compile file

--- compile.sh
+++ compile.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm -i -v $PWD -w /app golang:1.16 bash -c 'echo $PWD && ls -a && go mod init go.mod &&  go mod tidy && go get -u github.com/go-sql-driver/mysql && CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o app .'
+docker run --rm -i -v $PWD:/app -w /app golang:1.16 bash -c 'echo $PWD && ls -a && go mod init go.mod &&  go mod tidy && go get -u github.com/go-sql-driver/mysql && CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o app .'
