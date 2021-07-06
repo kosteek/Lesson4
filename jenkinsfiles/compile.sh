86a486890595b74e79b553ad05a83c85bae037ef
kosteek <kosteek911@gmail.com>
2021-07-07 00:15:50 +0300

    update deploy

--- jenkinsfiles/compile.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker run --rm -i -v $PWD:/app -w /app golang:1.16 bash -c 'go mod init go.mod &&  go mod tidy && go get -u github.com/go-sql-driver/mysql && CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o app .'
